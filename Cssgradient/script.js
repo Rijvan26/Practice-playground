7d4d772c98e4eb4f2c957b522c9693e43b9d432b
Rijvaan <khanrijvan2610@gmail.com>
2026-03-04 01:55:32 -0800

    added copy-clibboard and improve code readibility

--- Cssgradient/script.js
+++ Cssgradient/script.js
@@ -2,6 +2,10 @@ const main = document.querySelector('main')
 const para = document.querySelector('#para')
 const btn = document.querySelector('#changebtn')
 const btn2 = document.querySelector('#changebtn2')
+const copytext = document.querySelector('#copypara')
+let rgb1 = '#000'
+let rgb2 = "#fff"
+
 
 function myhexValue ()  {
    const data = '0123456789abcdef'
@@ -13,22 +17,25 @@ color =  color + data[Math.floor(Math.random()*16)]
    return color
 }
 
-const rgb1 = () => {
-   const rgb1val =  myhexValue()
-   const rgb2val =  myhexValue()
+const btnhandler1 = () => {
+    rgb1 =  myhexValue()
 
-    btn.innerHTML = rgb1val
-main.style.background = `linear-gradient(to right , ${rgb1val} ,${rgb2val}) `
-
-}
-const rgb2 = () => {
-   const rgb1val =  myhexValue()
-   const rgb2val =  myhexValue()
-    btn2.innerHTML = rgb2val
-main.style.background = `linear-gradient(to right , ${rgb1val} ,${rgb2val})`
-
+    btn.innerHTML = rgb1
+main.style.background = `linear-gradient(to right , ${rgb1} ,${rgb2}) `
+copytext.innerHTML = `background: linear-gradient(to right , ${rgb1} ,${rgb2})`
 
 }
+const btnhandler2 = () => {
+   rgb2 = myhexValue()
+    btn2.innerHTML = rgb2
+main.style.background = `linear-gradient(to right , ${rgb1} ,${rgb2})`
+copytext.innerHTML = `background: linear-gradient(to right , ${rgb1} ,${rgb2})`
 
-btn.addEventListener("click", rgb1)
-btn2.addEventListener("click", rgb2)
+}
+
+copytext.addEventListener("click", () => {
+   navigator.clipboard.writeText(copytext.innerText)
+})
+
+btn.addEventListener("click", btnhandler1)
+btn2.addEventListener("click", btnhandler2)
